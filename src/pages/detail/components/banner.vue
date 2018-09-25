95b745c18bda2c02f1d444ee1495b56f5e28569c
zhouyunpengt <1824584167@qq.com>
2018-09-25 11:25:24 +0800

    更新内容

---
+++ src/pages/detail/components/banner.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="banner">
+    <img class="banner-img" src="">
+    <div class="banner-info">
+      <div class="banner-title"></div>
+      <div class="banner-number">
+        <span class="iconfont banner-icon">&#xe692;</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+
+<style lang="stylus" scoped>
+.banner
+  position relative
+  overflow hidden
+  height 0
+  padding-bottom 55%
+  color #fff
+  .banner-img
+    width 100%
+  .banner-info
+    position absolute
+    left 0
+    right 0
+    bottom 0
+    line-height .6rem
+    display flex
+    background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+    .banner-title
+      font-size .32rem
+      padding 0 .2rem
+      flex 1
+    .banner-number
+      margin-top .14rem
+      padding 0 .4rem
+      line-height .32rem
+      height .4rem
+      border-radius .2rem
+      background rgba(0, 0, 0, .8)
+      font-size .24rem
+      .banner-icon
+        font-size .24rem
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import state from './state'
+import mutations from './mutations'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    city: '北京'
-  }
+  state: state,
+  mutations
 })
